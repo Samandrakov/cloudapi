75405aed50a392b064638502b27fc89ee475feaa
Danila Samandrakov <samandrakovda@gmail.com>
2026-01-22 02:33:41 +0300

    initial_commit

---
+++ output/test.py
@@ -0,0 +1,82 @@
+#coding=utf8
+import argparse
+
+import grpc
+
+import yandex.cloud.ai.stt.v3.stt_pb2 as stt_pb2
+import yandex.cloud.ai.stt.v3.stt_service_pb2_grpc as stt_service_pb2_grpc
+
+CHUNK_SIZE = 4000
+
+def gen(audio_file_name):
+    # Задайте настройки распознавания.
+    recognize_options = stt_pb2.StreamingOptions(
+        recognition_model=stt_pb2.RecognitionModelOptions(
+            audio_format=stt_pb2.AudioFormatOptions(
+                raw_audio=stt_pb2.RawAudio(
+                    audio_encoding=stt_pb2.RawAudio.LINEAR16_PCM,
+                    sample_rate_hertz=8000,
+                    audio_channel_count=1
+                )
+            ),
+            text_normalization=stt_pb2.TextNormalizationOptions(
+                text_normalization=stt_pb2.TextNormalizationOptions.TEXT_NORMALIZATION_ENABLED,
+                profanity_filter=True,
+                literature_text=False
+            ),
+            language_restriction=stt_pb2.LanguageRestrictionOptions(
+                restriction_type=stt_pb2.LanguageRestrictionOptions.WHITELIST,
+                language_code=['ru-RU']
+            ),
+            audio_processing_type=stt_pb2.RecognitionModelOptions.REAL_TIME
+        )
+    )
+
+    # Отправьте сообщение с настройками распознавания.
+    yield stt_pb2.StreamingRequest(session_options=recognize_options)
+
+    # Прочитайте аудиофайл и отправьте его содержимое порциями.
+    with open(audio_file_name, 'rb') as f:
+        data = f.read(CHUNK_SIZE)
+        while data != b'':
+            yield stt_pb2.StreamingRequest(chunk=stt_pb2.AudioChunk(data=data))
+            data = f.read(CHUNK_SIZE)
+
+# Вместо iam_token передавайте api_key при авторизации с API-ключом
+# от имени сервисного аккаунта.
+def run(api_key, audio_file_name):
+# def run(iam_token, audio_file_name):
+    # Установите соединение с сервером.
+    cred = grpc.ssl_channel_credentials()
+    channel = grpc.secure_channel('stt.api.cloud.yandex.net:443', cred)
+    stub = stt_service_pb2_grpc.RecognizerStub(channel)
+
+    # Отправьте данные для распознавания.
+    it = stub.RecognizeStreaming(gen(audio_file_name), metadata=(
+    # Параметры для авторизации с IAM-токеном
+    #     ('authorization', f'Bearer {iam_token}'),
+    # Параметры для авторизации с API-ключом от имени сервисного аккаунта
+      ('authorization', f'Api-Key {api_key}'),
+    ))
+
+    # Обработайте ответы сервера и выведите результат в консоль.
+    try:
+        for r in it:
+            event_type, alternatives = r.WhichOneof('Event'), None
+            if event_type == 'partial' and len(r.partial.alternatives) > 0:
+                alternatives = [a.text for a in r.partial.alternatives]
+            if event_type == 'final':
+                alternatives = [a.text for a in r.final.alternatives]
+            if event_type == 'final_refinement':
+                alternatives = [a.text for a in r.final_refinement.normalized_text.alternatives]
+            print(f'type={event_type}, alternatives={alternatives}')
+    except grpc._channel._Rendezvous as err:
+        print(f'Error code {err._state.code}, message: {err._state.details}')
+        raise err
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--token', required=True, help='IAM token or API key')
+    parser.add_argument('--path', required=True, help='audio file path')
+    args = parser.parse_args()
+    run(args.token, args.path)
